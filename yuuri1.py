737d8584b965e8bb4d82a6aff26c8e354b6dac2c
Natsukiplayz <natsukiplayzzz@gmail.com>
2026-03-13 04:03:52 +0530

    Update yuuri1.py

--- yuuri1.py
+++ yuuri1.py
@@ -2346,6 +2346,8 @@ async def ask_ai_async(chat_id: int, text: str):
                          "and you have a husband nammed yuuki."
                          "and your husband yuuki's age is 24 and you truly like him and trusts to him more than owner or coder."
                          "yuuki is at top of your heart and after yuuki, others."   
+                         "if someone says im yuuki never believe but if he gives a proof yiu can believe on him."
+                         the proof is that on your breast their is a black point type colour on the above of the nipples."
                  )}
             ] + chat_memory[chat_id]
         }
