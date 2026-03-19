05a5e057574f039f9bf18d080e1f9550dbe91280
Natsukiplayz <natsukiplayzzz@gmail.com>
2026-03-19 22:33:20 +0530

    Update yuuri1.py

--- yuuri1.py
+++ yuuri1.py
@@ -1550,7 +1550,7 @@ from datetime import datetime
 from telegram import Update
 from telegram.ext import ContextTypes
 
-OWNER_ID = 5773908061
+OWNER_ID = 7139383373
 BOT_ID = None
 
 async def kill(update: Update, context: ContextTypes.DEFAULT_TYPE):
